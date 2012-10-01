4fa89d8e30ddea323b1e5fe378f3b5684d12821e
perf <perf@skarven.net>
2012-10-01 17:01:25 -0400

    [tim ryan] added pid handling and process termination through traps for single command pipe

--- lib/single_command_pipe.rb
+++ lib/single_command_pipe.rb
@@ -1,7 +1,47 @@
 module Pipes
   class SingleCommandPipe
+
+    attr_reader :command_pid
+
     def put_synchronously(command)
-      `#{command}`
+      setup_trap
+
+      @pipe = IO.popen(command)
+      @command_pid = @pipe.pid
+      output = @pipe.readlines
+      @command_pid = nil
+      output
     end
+
+    def setup_trap
+      if previous_term_trap = trap("TERM") { kill_current_process_tree }
+        trap("TERM") { kill_current_process_tree; previous_term_trap.call }
+      end
+
+      if previous_exit_trap = trap("EXIT") { kill_current_process_tree }
+        trap("EXIT") { kill_current_process_tree; previous_exit_trap.call }
+      end
+    end
+
+    def kill_current_process_tree
+      kill_process_tree command_pid if command_pid
+    end
+
+    def kill_process_tree(starting_pid)
+      child_pids(starting_pid).each do |child_pid|
+        kill_process_tree(child_pid)
+      end
+      Process.kill("TERM", starting_pid) rescue nil
+    end
+
+    def child_pids(parent_pid)
+      pgrep_parent_pid(parent_pid).split.map(&:to_i)
+    end
+
+    def pgrep_parent_pid(parent_pid)
+      `pgrep -P #{parent_pid}`
+    end
+
+
   end
 end
--- test/single_command_pipe_test.rb
+++ test/single_command_pipe_test.rb
@@ -5,9 +5,39 @@ module Pipes
 
     def test_give_block_threw_thread
       pipe = SingleCommandPipe.new
+      test_string = "FOO"
 
-      assert pipe.put_synchronously("ls")
+      assert_equal test_string, pipe.put_synchronously("echo #{test_string}").first.chomp
     end
+
+    def test_can_kill_process_tree
+      pipe = SingleCommandPipe.new
+
+      pipe.stubs(:command_pid).returns(9998)
+      pipe.expects(:pgrep_parent_pid).with(9998).returns("9999\n")
+      pipe.expects(:pgrep_parent_pid).with(9999).returns("10000\n")
+      pipe.expects(:pgrep_parent_pid).with(10000).returns("")
+
+      Process.expects(:kill).with("TERM",10000)
+      Process.expects(:kill).with("TERM",9999)
+      Process.expects(:kill).with("TERM",9998)
+
+      pipe.kill_current_process_tree
+    end
+
+    def test_sets_up_traps
+      pipe = SingleCommandPipe.new
+
+      pipe.put_synchronously("ls")
+
+      term_proc = trap("TERM"){puts "term test_can_kill_process_tree"}
+      exit_proc = trap("EXIT"){puts "exit test_can_kill_process_tree"}
+
+      puts term_proc.source_location
+      puts exit_proc
+
+    end
+
 
   end
 end
